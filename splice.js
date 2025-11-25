561183f69e3919736ebe99234aab354e7b788923
ksaini-web <kartiksaini9084@gmail.com>
2025-11-25 14:10:18 +0530

    Add examples of array splice method
    Demonstrate usage of splice method on an array.

---
+++ splice.js
@@ -0,0 +1,21 @@
+
+const arr2=["rohit",24,"mern","image"];
+//splice(starting_index,remove_count,add_item);
+
+arr2.splice(2,1,"kartik",24,"ducat");
+
+console.log(arr2);
+
+//remove item;
+arr2.splice(2,3);
+
+//without remove add element;
+
+arr2.splice(2,0,"hello");
+console.log(arr2);
+
+
+
+
+
+
